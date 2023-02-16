043f90e814a7d7d1a7af52f6c43f8755638b3b66
Jelmer Draaijer <info@jelmert.nl>
2023-02-16 10:10:36 +0100

    Fix async io tests

--- tests/test_async_dossiers.py
+++ tests/test_async_dossiers.py
@@ -5,9 +5,9 @@ from checkedid.client import ClientAsync
 
 @pytest.mark.asyncio
 async def test_dossiers(customer_code, dossier_response_200):
-    client = ClientAsync(customer_code)
+    checkedid = ClientAsync(customer_code)
 
-    with client as client:
-        response = await client.adossier("999999-8888800")
+    async with checkedid as client:
+        response = await client.dossier("999999-8888800")
 
         assert response.DossierNumber == "999999-8888800"
